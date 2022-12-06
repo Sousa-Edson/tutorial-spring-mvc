7b9bc6256efd7aad132e2556153630fde71e23ce
Edson Sousa <edson3711.es@gmail.com>
2022-12-06 20:51:02 -0300

    Update HelloController.java

--- src/main/java/com/edson/tutorialspringmvc/controller/HelloController.java
+++ src/main/java/com/edson/tutorialspringmvc/controller/HelloController.java
@@ -1,13 +1,37 @@
 package com.edson.tutorialspringmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloController {
+	
 	@GetMapping("/hello")
 	public String hello() {
 		return "hello";
+	}
+	
+	@GetMapping("/hello-servlet")
+	public String hello(HttpServletRequest request) {
+		request.setAttribute( "nome", "tetse de servetl");
+		return "hello";
+	}
+	
+	@GetMapping("/hello-model")
+	public String hello2(Model model) {
+		model.addAttribute("nome", "modelo");
+		return "hello";
+	}
+	
+	@GetMapping("/hello-final")
+	public ModelAndView hello3(Model model) {
+		ModelAndView mv = new ModelAndView("hello");
+		mv.addObject("nome", "model and view");
+		return mv;
 	}
 
 }
