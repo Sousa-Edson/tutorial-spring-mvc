064ca471619c55c4c8ea76798b24208e42c9f32f
Edson Sousa <edson3711.es@gmail.com>
2022-12-06 22:38:36 -0300

    Update Professor.java

--- src/main/java/com/edson/tutorialspringmvc/Professor.java
+++ src/main/java/com/edson/tutorialspringmvc/Professor.java
@@ -14,16 +14,23 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Professor {
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-@Column(nullable = false)
+	@Column(nullable = false)
 	private String nome;
 	private BigDecimal salario;
 	@Enumerated(EnumType.STRING)
 	private StatusProfessor statusProfessor;
 
 	public Professor() {
+	}
+
+	public Professor(String nome, BigDecimal salario, StatusProfessor statusProfessor) {
+		super();
+		this.nome = nome;
+		this.salario = salario;
+		this.statusProfessor = statusProfessor;
 	}
 
 	public long getId() {
