a55091b2f0d7e1ad59db5dab6ca2375dfbe08478
Edson Sousa <edson3711.es@gmail.com>
2022-12-06 22:53:40 -0300

    Update ProfessorController.java

--- src/main/java/com/edson/tutorialspringmvc/controller/ProfessorController.java
+++ src/main/java/com/edson/tutorialspringmvc/controller/ProfessorController.java
@@ -1,30 +1,26 @@
 package com.edson.tutorialspringmvc.controller;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.edson.tutorialspringmvc.Professor;
-import com.edson.tutorialspringmvc.model.StatusProfessor;
+import com.edson.tutorialspringmvc.ProfessorRepository;
 
 @Controller
 public class ProfessorController {
+	@Autowired
+	private ProfessorRepository professorRepository;
+
 	@GetMapping("/professores")
 	public ModelAndView index() {
-		Professor batman =  new Professor("Batman",new BigDecimal(20.500),StatusProfessor.ATIVO);
-		batman.setId(1L);
-		Professor coringa =  new Professor("coringa",new BigDecimal(57.500),StatusProfessor.APOSENTADO);
-		coringa.setId(2L);
-		Professor mulherMaravilha =  new Professor("mulherMaravilha",new BigDecimal(100.00),StatusProfessor.AFASTADO);
-		mulherMaravilha.setId(3L);
-		
-		List <Professor> professores= Arrays.asList(batman,coringa,mulherMaravilha);
+	 
+		List<Professor> professores =  this.professorRepository.findAll();
 		ModelAndView mv = new ModelAndView("professores/index");
-		mv.addObject("professores",professores);
+		mv.addObject("professores", professores);
 		return mv;
 	}
 }
