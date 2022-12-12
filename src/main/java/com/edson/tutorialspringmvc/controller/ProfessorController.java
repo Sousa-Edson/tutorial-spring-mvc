fd774fd9b80058dcac1e473178724e407cca9c75
Edson Sousa <edson3711.es@gmail.com>
2022-12-12 07:33:15 -0300

    inserindo detalhe

--- src/main/java/com/edson/tutorialspringmvc/controller/ProfessorController.java
+++ src/main/java/com/edson/tutorialspringmvc/controller/ProfessorController.java
@@ -1,11 +1,13 @@
 package com.edson.tutorialspringmvc.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -38,16 +40,31 @@ public class ProfessorController {
 
 	@PostMapping("/professores")
 	public ModelAndView create(@Valid RequisicaoNovoProfessor requisicao, BindingResult bindingResult) {
-		  System.out.println(requisicao);
+		System.out.println(requisicao);
 		if (bindingResult.hasErrors()) {
 			System.out.println("\n************************TEM ERROS**********************\n");
 			ModelAndView mv = new ModelAndView("professores/new");
-			
+
 			mv.addObject("listaStatusProfessor", StatusProfessor.values());
 			return mv;
 		} else {
 			Professor professor = requisicao.toProfessor();
 			this.professorRepository.save(professor);
+			return new ModelAndView("redirect:/professores/"+professor.getId());
+		}
+	}
+
+	@GetMapping("/professores/{id}")
+	public ModelAndView show(@PathVariable Long id) {
+		System.out.println("**** ID: " + id);
+		Optional<Professor> optional = this.professorRepository.findById(id);
+		if (optional.isPresent()) {
+			Professor professor = optional.get();
+			ModelAndView mv = new ModelAndView("professores/show");
+			mv.addObject(professor);
+			return mv;
+		} else {
+			System.out.println("$$$$$$$$$$$ Não achou o professor");
 			return new ModelAndView("redirect:/professores");
 		}
 	}
