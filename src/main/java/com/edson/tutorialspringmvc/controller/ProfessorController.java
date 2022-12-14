063ed1b8a914c2af3e714ca2d097c5d3bd328912
Edson Sousa <edson3711.es@gmail.com>
2022-12-13 21:16:51 -0300

    Update ProfessorController.java

--- src/main/java/com/edson/tutorialspringmvc/controller/ProfessorController.java
+++ src/main/java/com/edson/tutorialspringmvc/controller/ProfessorController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -67,7 +68,7 @@ public class ProfessorController {
 			return mv;
 		} else {
 			System.out.println("$$$$$$$$$$$ Não achou o professor");
-			return new ModelAndView("redirect:/professores");
+			return this.retornaErroProfessor("SHOW ERROR: Professor #" + id + " não encontrado no banco!");
 		}
 	}
 
@@ -106,9 +107,32 @@ public class ProfessorController {
 				return new ModelAndView("redirect:/professores/" + professor.getId());
 			} else {
 				System.out.println("########### Não achou o professor");
-				return new ModelAndView("redirect:/professores");
+				return this.retornaErroProfessor("UPDATE ERROR: Professor #" + id + " não encontrado no banco!");
 			}
 		}
 
+	}
+
+	@GetMapping("/{id}/delete")
+	public ModelAndView delete(@PathVariable Long id) {
+		ModelAndView mv = new ModelAndView("redirect:/professores");
+
+		try {
+			this.professorRepository.deleteById(id);
+			mv.addObject("mensagem", "Professor #" + id + " deletado com sucesso!");
+			mv.addObject("erro", false);
+		} catch (EmptyResultDataAccessException e) {
+			System.out.println(e);
+			mv = this.retornaErroProfessor("DELETE ERROR: Professor #" + id + " não encontrado no banco!");
+		}
+
+		return mv;
+	}
+
+	private ModelAndView retornaErroProfessor(String msg) {
+		ModelAndView mv = new ModelAndView("redirect:/professores");
+		mv.addObject("mensagem", msg);
+		mv.addObject("erro", true);
+		return mv;
 	}
 }
